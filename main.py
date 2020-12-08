11fa87b4c551bbb3c86cdcf75ce3b83898a878c3
Spimy <officialspyme@gmail.com>
2020-12-08 22:43:36 +0400

    Add day selector

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+days = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d) and d != '.git']
+paths = [os.path.join(os.getcwd(), day) for day in days]
+
+for index, day in enumerate(days):
+    print(f'{index + 1}. {day}')
+
+while True:
+    selected_day = int(input('Run script for day: '))
+
+    if selected_day > len(days) or selected_day < len(days):
+        print('Exiting for entering an inexistent day...')
+        break
+
+    exec(open(f'{paths[selected_day - 1]}\\main.py').read())
+    print()
